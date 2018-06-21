adc390298f86a3f0762883a9e67b084a0343d71e
Txuba <txuba.costa@gmail.com>
2018-06-21 00:59:18 -0300

    Update grafo.c

--- src/grafo.c
+++ src/grafo.c
@@ -6,8 +6,19 @@ void free_grafo(grafo * grafico)
 	for (i = 0; i < grafico->tamanho_grafo; i++)
 	{
 		free(grafico->nos_grafo[i].nos_vizinhos);
+		free(grafico->nos_grafo[i];
 	}
 	free(grafico->nos_grafo);
 	free(grafico);
 	return;
+}
+
+int get_indice_no_numero(grafo* rede, int valor){
+	int i;	
+	for(i=0; i<rede->tamanho_grafo;i++){
+		if(rede->nos_grafo[i].valor_no == valor){
+			return i;
+		}
+	}
+	return -1;
 }
