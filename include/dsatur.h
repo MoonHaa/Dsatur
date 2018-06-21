e91f3a3d94d440c2dcba7df398b12529ff35814f
Txuba <txuba.costa@gmail.com>
2018-06-21 02:02:56 -0300

    taí seu commit

---
+++ include/dsatur.h
@@ -0,0 +1,11 @@
+#ifndef DSATUR_H_
+#define DSATUR_H_
+
+int DSATUR(grafo* rede);
+void colorir(grafo* rede, int indice, int* maior_cor);
+void inicializar_grafo(grafo* rede);
+int indice_proximo_colorir(grafo* rede);
+int is_todos_coloridos(grafo* rede);
+void atualizar_dsatur_e_vizinhos(grafo* rede, int indice);
+
+#endif // !DSATUR_H_
